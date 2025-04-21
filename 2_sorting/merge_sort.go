b8c8ddad67842f0490786fe125b39cf52cc9e534
Akansh Gupta <akansh.gupta1298@gmail.com>
2025-04-21 19:34:28 +0530

    Completed merge sort

--- 2_sorting/merge_sort.go
+++ 2_sorting/merge_sort.go
@@ -5,8 +5,8 @@ import "fmt"
 func mergeSort(arr []int, start, end int) {
 	low := start
 	high := end
-	mid := low + ((high - low) / 2)
-	if mid == low {
+	mid := (low + high) / 2
+	if low >= high {
 		return
 	}
 	mergeSort(arr, low, mid)
@@ -17,20 +17,42 @@ func mergeSort(arr []int, start, end int) {
 }
 func merge(arr []int, start, mid, end int) {
 	tmp := []int{}
+	i, j := start, mid+1
+	for i <= mid && j <= end {
+		if arr[i] < arr[j] {
+			tmp = append(tmp, arr[i])
+			// tmp[k] = arr[i]
+			i++
+		} else {
+			tmp = append(tmp, arr[j])
+			j++
+		}
+		// k++
+	}
+
+	for i <= mid {
+		tmp = append(tmp, arr[i])
+		// k++
+		i++
+	}
+
+	for j <= end {
+		tmp = append(tmp, arr[j])
+		j++
+	}
+
+	for i = start; i <= end; i++ {
+		arr[i] = tmp[i-start]
+
+	}
 
 }
 
 func main() {
 
-	arr := []int{2, 1, 3, 4} //, 3, 4, 5, 15, 23, 11, 3, 4, 5}
-	// low := 0
-	// high := len(arr)
-	// mid := (high - low) / 2
+	arr := []int{2, 1, 3, 4, 3, 4, 5, 15, 23, 11, 3, 4, 5}
 	fmt.Println("Original: ", arr)
-	mergeSort(arr, 0, len(arr))
-	// mergeSort(arr, mid+1, high)
-	// merge(arr)
-
+	mergeSort(arr, 0, len(arr)-1)
 	fmt.Println(arr)
 
 }
